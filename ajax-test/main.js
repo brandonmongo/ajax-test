c38fb125ceb3e41136252d1ada0f4d5343ff92d7
brandonmongo <brandonmongo@hotmail.com>
2020-04-03 19:28:27 +0000

    Pagination lesson complete

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -1,41 +1,57 @@
-const baseURL = "https://swapi.co/api/";
 
-function getData(type, cb) {
+function getData(url, cb) {
     let xhr = new XMLHttpRequest();
 
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             cb(JSON.parse(this.responseText));
         }
     };
 
-    xhr.open("GET", baseURL + type + "/");
+    xhr.open("GET", url);
     xhr.send();
+}
+
+function generatePaginationButtons(next, prev) {
+    if (next && prev) {
+        return `<button onclick = "writeToDocument('${prev}')">Previous</button>;
+            <button onclick = "writeToDocument('${next}')">Next</button>`;
+    } else if (next && !prev) {
+        return `<button onclick = "writeToDocument('${next}')">Next</button>`;
+    } else if (!next && prev) {
+        return `<button onclick = "writeToDocument('${prev}')">Previous</button>`;
+    }
+
 }
 
 function getTableHeaders(obj) {
     let tableHeaders = [];
 
-    Object.keys(obj).forEach(function(key) {
+    Object.keys(obj).forEach(function (key) {
         tableHeaders.push(`<td>${key}</td>`)
     });
 
     return `<tr>${tableHeaders}</tr>`;
 }
 
-function writeToDocument(type) {
+function writeToDocument(url) {
     let tableRows = [];
     let el = document.getElementById("data");
     el.innerHTML = "";
 
-    getData(type, function(data) {
+    getData(url, function (data) {
+        let pageination;
+        if (data.next || data.previous) {
+            pageination = generatePaginationButtons(data.next, data.previous)
+        }
+
         data = data.results;
         let tableHeaders = getTableHeaders(data[0]);
 
-        data.forEach(function(item) {
+        data.forEach(function (item) {
             let dataRow = [];
 
-            Object.keys(item).forEach(function(key) {
+            Object.keys(item).forEach(function (key) {
                 let rowData = item[key].toString();
                 let truncatedData = rowData.substring(0, 15);
                 dataRow.push(`<td>${truncatedData}</td>`)
@@ -43,6 +59,6 @@ function writeToDocument(type) {
             tableRows.push(`<tr>${dataRow}</tr>`)
         });
 
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pageination}`;
     });
 }
